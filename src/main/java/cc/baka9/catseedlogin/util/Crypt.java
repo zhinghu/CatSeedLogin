ef237fe3a77edf447cc85f55b9602ab8caefc4a3
Shuiling <1668589925@qq.com>
2024-06-08 10:29:24 -0700

    feat(Crypt): 重构加密方法增加密码明文拼接过程

--- src/main/java/cc/baka9/catseedlogin/util/Crypt.java
+++ src/main/java/cc/baka9/catseedlogin/util/Crypt.java
@@ -9,7 +9,9 @@ public class Crypt {
     private static final char[] CRYPTCHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     public static String encrypt(final String name, final String password){
-        final String text = "ÜÄaeut//&/=I " + password + "7421€547" + name + "__+IÄIH§%NK " + password;
+        StringBuilder sb = new StringBuilder();
+        sb.append("ÜÄaeut//&/=I ").append(password).append("7421€547").append(name).append("__+IÄIH§%NK ").append(password);
+        String text = sb.toString();
         try {
             final MessageDigest md = MessageDigest.getInstance("SHA-512");
             md.update(text.getBytes(StandardCharsets.UTF_8), 0, text.length());
@@ -19,7 +21,7 @@ public class Crypt {
         }
     }
 
-    public static String byteArrayToHexString(byte... args){
+    public static String byteArrayToHexString(byte[] args){
         char[] chars = new char[args.length * 2];
         for (int i = 0; i < args.length; i++) {
             chars[(i * 2)] = CRYPTCHARS[(args[i] >> 4 & 0xF)];
