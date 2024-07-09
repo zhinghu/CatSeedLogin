35e5b5d252c7c659a16c6f8b9d4ddb9d7707b329
Shuiling <1668589925@qq.com>
2024-07-09 04:07:36 -0700

    feat(database): 优化SQLite数据库连接获取方式

--- src/main/java/cc/baka9/catseedlogin/bukkit/database/SQLite.java
+++ src/main/java/cc/baka9/catseedlogin/bukkit/database/SQLite.java
@@ -1,10 +1,10 @@
 package cc.baka9.catseedlogin.bukkit.database;
 
-import org.bukkit.plugin.java.JavaPlugin;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class SQLite extends SQL {
     private Connection connection;
@@ -13,26 +13,18 @@ public class SQLite extends SQL {
         super(javaPlugin);
     }
 
-
     @Override
-public Connection getConnection() {
-    try {
-        if (this.connection != null && !this.connection.isClosed()) {
-            return this.connection;
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                if (!plugin.getDataFolder().exists()) plugin.getDataFolder().mkdir();
+                Class.forName("org.sqlite.JDBC");
+                connection = DriverManager.getConnection("jdbc:sqlite:" + plugin.getDataFolder().getAbsolutePath() + "/accounts.db");
+            }
+            return connection;
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return null;
         }
-        
-        if (!plugin.getDataFolder().exists()) {
-            plugin.getDataFolder().mkdir();
-        }
-        
-        Class.forName("org.sqlite.JDBC");
-        this.connection = DriverManager.getConnection("jdbc:sqlite:" + plugin.getDataFolder().getAbsolutePath() + "/accounts.db");
-        
-        return this.connection;
-    } catch (ClassNotFoundException | SQLException e) {
-        e.printStackTrace();
-        return null;
     }
-}
-
 }
