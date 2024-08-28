4cd8b75a465445405371d55e74c1bcdf75a673e4
Shuiling <132723284+shulng@users.noreply.github.com>
2024-08-28 17:26:29 +0800

    fix: 修复FloodgatePrefixProtect名称大小写问题 (#20)

--- src/main/java/cc/baka9/catseedlogin/util/Mail.java
+++ src/main/java/cc/baka9/catseedlogin/util/Mail.java
@@ -26,8 +26,7 @@ public class Mail {
         email.setSubject(subject);
         email.setMsg(content);
         email.addTo(receiveMailAccount);
-        ((HtmlEmail) email).setCharset("UTF-8");
-        ((HtmlEmail) email).setCharset("GBK");
+        email.setCharset("UTF-8");
         email.send();
 
     }
