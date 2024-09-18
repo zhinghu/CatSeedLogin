7863d28d46048e7c0c8701251080bbc265c3a092
Shuiling <1668589925@qq.com>
2024-09-18 05:53:12 -0700

    feat(Communication): 重构通讯代码，优化异常处理

--- src/main/java/cc/baka9/catseedlogin/bungee/Communication.java
+++ src/main/java/cc/baka9/catseedlogin/bungee/Communication.java
@@ -1,12 +1,12 @@
 package cc.baka9.catseedlogin.bungee;
 
-import java.net.Socket;
-import java.io.IOException;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.net.Socket;
 
-import net.md_5.bungee.api.ProxyServer;
 import cc.baka9.catseedlogin.util.CommunicationAuth;
+import net.md_5.bungee.api.ProxyServer;
 
 /**
  * bc 与 bukkit 的通讯交流
@@ -16,45 +16,26 @@ public class Communication {
     private static final int PORT = Config.Port;
 
     public static int sendConnectRequest(String playerName) {
-        Socket socket = null;
-        BufferedWriter writer = null;
-        try {
-            socket = getSocket();
-            writer = getSocketBufferedWriter(socket);
+        try (Socket socket = new Socket(HOST, PORT);
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
             writeMessage(writer, "Connect", playerName);
             return socket.getInputStream().read();
         } catch (IOException e) {
-            handleIOException(e);
+            ProxyServer.getInstance().getLogger().severe("发生 I/O 异常: " + e.getMessage());
             return 0;
         }
     }
 
     public static void sendKeepLoggedInRequest(String playerName) {
-        Socket socket = null;
-        BufferedWriter writer = null;
-        try {
-            socket = getSocket();
-            writer = getSocketBufferedWriter(socket);
+        try (Socket socket = new Socket(HOST, PORT);
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
             long currentTime = System.currentTimeMillis();
             String time = String.valueOf(currentTime);
             String sign = CommunicationAuth.encryption(playerName, time, Config.AuthKey);
             writeMessage(writer, "KeepLoggedIn", playerName, time, sign);
         } catch (IOException e) {
-            handleIOException(e);
+            ProxyServer.getInstance().getLogger().severe("发生 I/O 异常: " + e.getMessage());
         }
-    }
-
-    private static Socket getSocket() throws IOException {
-        try {
-            return new Socket(HOST, PORT);
-        } catch (IOException e) {
-            ProxyServer.getInstance().getLogger().warning("§c请检查装载登录插件的子服是否在 bungeecord.yml 中开启了bungeecord功能，以及Host和Port是否与bc端的配置相同");
-            throw e;
-        }
-    }
-
-    private static BufferedWriter getSocketBufferedWriter(Socket socket) throws IOException {
-        return new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
     }
 
     private static void writeMessage(BufferedWriter writer, String... messages) throws IOException {
@@ -63,9 +44,5 @@ public class Communication {
             writer.newLine();
         }
         writer.flush();
-    }
-
-    private static void handleIOException(IOException e) {
-        ProxyServer.getInstance().getLogger().severe("发生 I/O 异常: " + e.getMessage());
     }
 }
