1b3dc555ab1bdc6bbcd30e30458f776c2657a93f
Shuiling <1668589925@qq.com>
2024-09-20 05:49:27 -0700

    feat(Config): 更新配置文件和日志输出

--- src/main/java/cc/baka9/catseedlogin/bungee/Config.java
+++ src/main/java/cc/baka9/catseedlogin/bungee/Config.java
@@ -4,11 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
-import java.util.logging.Logger;
 
 import net.md_5.bungee.config.Configuration;
-import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
+import net.md_5.bungee.config.ConfigurationProvider;
 
 public class Config {
 
@@ -17,7 +16,6 @@ public class Config {
     public static int Port;
     public static String LoginServerName;
     public static String AuthKey;
-    private static final Logger logger = PluginMain.instance.getLogger();
 
     public static void load() {
         File dataFolder = PluginMain.instance.getDataFolder();
@@ -42,12 +40,9 @@ public class Config {
             Port = config.getInt("Port");
             LoginServerName = config.getString("LoginServerName");
             AuthKey = config.getString("AuthKey");
-
-            // 合并日志输出
-            String logBuilder = "Host: " + Host + "\n" +
-                    "Port: " + Port + "\n" +
-                    "LoginServerName: " + LoginServerName;
-            logger.info(logBuilder);
+            PluginMain.instance.getLogger().info("Host:" + Host);
+            PluginMain.instance.getLogger().info("Port:" + Port);
+            PluginMain.instance.getLogger().info("LoginServerName:" + LoginServerName);
         } catch (IOException e) {
             e.printStackTrace();
         }
