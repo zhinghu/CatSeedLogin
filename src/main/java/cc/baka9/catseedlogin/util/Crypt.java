0df75a53977ddc2d33dc1417a6d6fae309edb2ec
Shuiling <1668589925@qq.com>
2024-11-28 01:49:47 -0800

    微调

--- src/main/java/cc/baka9/catseedlogin/util/Crypt.java
+++ src/main/java/cc/baka9/catseedlogin/util/Crypt.java
@@ -5,10 +5,9 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class Crypt {
-
     private static final char[] CRYPTCHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
-    public static String encrypt(final String name, final String password){
+    public static String encrypt(final String name, final String password) {
         String text = "ÜÄaeut//&/=I " + password + "7421€547" + name + "__+IÄIH§%NK " + password;
         try {
             final MessageDigest md = MessageDigest.getInstance("SHA-512");
@@ -19,7 +18,7 @@ public class Crypt {
         }
     }
 
-    public static String byteArrayToHexString(byte[] args){
+    public static String byteArrayToHexString(byte[] args) {
         char[] chars = new char[args.length * 2];
         for (int i = 0; i < args.length; i++) {
             chars[(i * 2)] = CRYPTCHARS[(args[i] >> 4 & 0xF)];
@@ -28,12 +27,11 @@ public class Crypt {
         return new String(chars);
     }
 
-    public boolean match(final String name, final String password, final String encrypted){
+    public boolean match(final String name, final String password, final String encrypted) {
         try {
             return encrypted.equals(encrypt(name, password));
         } catch (final Exception e) {
             return false;
         }
     }
-
 }
--- src/main/java/cc/baka9/catseedlogin/util/Mail.java
+++ src/main/java/cc/baka9/catseedlogin/util/Mail.java
@@ -3,16 +3,12 @@ package cc.baka9.catseedlogin.util;
 import org.apache.commons.mail.DefaultAuthenticator;
 import org.apache.commons.mail.Email;
 import org.apache.commons.mail.HtmlEmail;
-
 import cc.baka9.catseedlogin.bukkit.Config;
 
 public class Mail {
-
-    private Mail() {
-    }
+    private Mail() {}
 
     public static void sendMail(String receiveMailAccount, String subject, String content) throws Exception {
-
         Email email = new HtmlEmail();
         email.setHostName(Config.EmailVerify.EmailSmtpHost);
         email.setSmtpPort(Integer.parseInt(Config.EmailVerify.EmailSmtpPort));
@@ -29,7 +25,5 @@ public class Mail {
         email.addTo(receiveMailAccount);
         email.setCharset("UTF-8");
         email.send();
-
     }
-
 }
