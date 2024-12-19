c26285dde03a9cb6c7442e7c546a966de4c3910f
Shuiling <1668589925@qq.com>
2024-12-19 02:45:58 -0800

    修复获取离线玩家位置逻辑

--- src/main/java/cc/baka9/catseedlogin/bukkit/Config.java
+++ src/main/java/cc/baka9/catseedlogin/bukkit/Config.java
@@ -282,8 +282,7 @@ public class Config {
 
     // 获取玩家退出服务器时的位置
     public static Optional<Location> getOfflineLocation(Player player) {
-        return Optional.ofNullable(offlineLocations.get(player.getName()))
-                       .map(Config::str2Location);
+        return Optional.ofNullable(plugin.getConfig().getString("offlineLocations." + player.getName())).map(Config::str2Location);
     }
 
     // 保存玩家退出服务器的位置
