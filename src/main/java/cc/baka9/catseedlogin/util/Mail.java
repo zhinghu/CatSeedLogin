9faaa41c2cee30f194a62821765e55d9eced82e0
Shuiling <1668589925@qq.com>
2025-01-29 23:52:32 -0800

    优化

--- src/main/java/cc/baka9/catseedlogin/util/Mail.java
+++ src/main/java/cc/baka9/catseedlogin/util/Mail.java
@@ -1,15 +1,15 @@
 package cc.baka9.catseedlogin.util;
 
 import org.apache.commons.mail.DefaultAuthenticator;
-import org.apache.commons.mail.Email;
 import org.apache.commons.mail.HtmlEmail;
+import org.apache.commons.mail.EmailException;
 import cc.baka9.catseedlogin.bukkit.Config;
 
 public class Mail {
     private Mail() {}
 
-    public static void sendMail(String receiveMailAccount, String subject, String content) throws Exception {
-        Email email = new HtmlEmail();
+    public static void sendMail(String receiveMailAccount, String subject, String content) throws EmailException {
+        HtmlEmail email = new HtmlEmail();
         email.setHostName(Config.EmailVerify.EmailSmtpHost);
         email.setSmtpPort(Integer.parseInt(Config.EmailVerify.EmailSmtpPort));
         email.setAuthenticator(new DefaultAuthenticator(Config.EmailVerify.EmailAccount, Config.EmailVerify.EmailPassword));
