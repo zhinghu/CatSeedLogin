b5859fa13c7c39e81aa162836d6648e29c1a602d
Shuiling <1668589925@qq.com>
2025-01-29 23:57:05 -0800

    移除Util类中冗余的随机数生成器

--- src/main/java/cc/baka9/catseedlogin/util/Util.java
+++ src/main/java/cc/baka9/catseedlogin/util/Util.java
@@ -1,17 +1,13 @@
 package cc.baka9.catseedlogin.util;
 
-import java.security.SecureRandom;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Random;
 import java.util.regex.Pattern;
 import org.apache.commons.lang3.RandomStringUtils;
 
 public class Util {
     private static final Pattern passwordDifficultyRegex = Pattern.compile("^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,16}$");
     private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-    private static final Random random = new SecureRandom();
-
     public static boolean passwordIsDifficulty(String pwd) {
         return !passwordDifficultyRegex.matcher(pwd).find();
     }
